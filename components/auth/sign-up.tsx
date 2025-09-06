28f87c165bbc02547ef87e9c352a246b237e0c6e
DevyManish <manishguptacpn@gmail.com>
2025-09-06 15:19:36 +0530

    fix: onSuccess to app

--- components/auth/sign-up.tsx
+++ components/auth/sign-up.tsx
@@ -168,7 +168,7 @@ export default function SignUp() {
                     toast.error(ctx.error.message);
                   },
                   onSuccess: async () => {
-                    router.push("/dashboard");
+                    router.push("/app");
                   },
                 },
               });
